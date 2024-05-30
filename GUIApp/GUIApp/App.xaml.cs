e91b573c3fae5405b8c91f318dd3b3eaa5f5e41d
Aahad143 <aahadm004@gmail.com>
2024-05-30 18:55:06 +0400

    Initializing project with C# WPF for GUI Developemnt
    This project is to create a machine learning model in python and a C++ GUI application where one of it's features is the ML model.

---
+++ GUIApp/GUIApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GUIApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
